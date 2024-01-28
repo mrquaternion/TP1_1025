7b3110e25641c4d2afd5c8838a7d5f157a90f791
mathiiias13 <mathias.la.rochelle@umontreal.ca>
2024-01-28 16:18:55 -0500

    Ajout constructeur de la classe Enemy ainsi que ses attributs

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,21 @@
 public class Enemy {
-    
+
+    // Attributs privés
+    private int pointDeVie = 10;
+    private int pointAttaque = 25;
+    private int pointExperience = 35;
+
+    // Constructeur
+    public Enemy(int pointDeVie, int pointAttaque, int pointExperience) {
+        this.pointDeVie = pointDeVie;
+        this.pointAttaque = pointAttaque;
+        this.pointExperience = pointExperience;
+    }
+
+    private void ameliorationStats() { // La fonction fight() de la classe héro appel cette fonction si le héro bat le monstre
+        this.pointDeVie += 10;
+        this.pointAttaque += 5;
+        this.pointExperience += 8;
+    }
+
 }
